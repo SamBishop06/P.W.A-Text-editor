71f38109a1eb67d28987e389a5375318c53f6169
SamBishop06 <samuelbishop06@yahoo.com>
2024-05-09 14:16:07 -0500

    fixing typos and structure

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,52 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: '.index.html',
+        title: 'J.A.T.E'
+      }),
+      new InjectManifest({
+        swSRC: './src-sw.js'
+        swDest: 'service-worker.js'
+      }),
+      new WebpackPwaManifest({
+        name: 'J.A.T.E',
+        short_name:'J.A.T.E',
+        description: 'Just Another Text Editor',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        start_url: './',
+        publicPath:'./',
+        icons:  [{   // Array of icons for the web app
+          src: path.resolve(__dirname, 'src/images/logo.png'),   
+          sizes: [96, 128, 192, 256, 384, 512],   
+          destination: path.join('assets', 'icons'),  
+        }],
+        
+
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,   
+          use: ['style-loader', 'css-loader']   
+        },
+        {
+          test: /\.m?js$/,  
+        exclude: /node_modules/, 
+          use: {
+            loader: 'babel-loader',   // Babel loader for JavaScript files
+            options: {
+              presets: ['@babel/preset-env'],   // Babel presets
+              plugins: [
+                '@babel/plugin-transform-object-rest-spread',   // Babel plugin for object spread
+                '@babel/transform-runtime'   // Babel plugin for runtime transformation
+              ]
+            }
+          }
+        }
       ],
     },
   };
